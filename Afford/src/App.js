d4c4153c85fbdd7145d5c6db384d80f71fc723f8
Shreyansh Shrivastava <154584697+shreyyy-333@users.noreply.github.com>
2025-04-18 11:55:29 +0530

    Add files via upload

---
+++ Afford/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, this is a minimal React app!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ Afford/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  mode: 'development',
+  devServer: {
+    static: './dist',
+    hot: true,
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
